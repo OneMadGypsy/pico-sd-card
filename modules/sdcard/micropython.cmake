a48dfcffaf79f745ef7170240c5df93dbc614e58
Michael Guidry <onemadgypsy@gmail.com>
2021-05-08 12:59:32 -0500

    Update micropython.cmake

--- modules/sdcard/micropython.cmake
+++ modules/sdcard/micropython.cmake
@@ -1,1 +1,15 @@
+add_library(usermod_sdcard INTERFACE)
 
+target_sources(usermod_sdcard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/sdcard.c
+)
+
+target_include_directories(usermod_sdcard INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+target_compile_definitions(usermod_sdcard INTERFACE
+    -DMODULE_SDCARD_ENABLED=1
+)
+
+target_link_libraries(usermod INTERFACE usermod_sdcard)
